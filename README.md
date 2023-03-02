9cb32726b782d4abe0cadc4be43158c153b4e9c3
chavda-bhavik <bhavikvchavda@gmail.com>
2023-03-02 13:47:11 +0530

    setup commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Requirements
+- [Live Server Extention](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)
---
+++ script.js
@@ -0,0 +1,22 @@
+// function getUsers() {
+//   return new Promise((res, rej) => {
+//     setTimeout(() => {
+//       res([]);
+//     }, 1000);
+//   });
+// }
+// function getUsers() {
+//   return new Promise((res, rej) => {
+//     setTimeout(() => {
+//       rej(new Error("Something went wrong"));
+//       rej("Something went wrong");
+//     }, 1000);
+//   });
+// }
+
+// async function main() {
+//   console.log("Execution is started");
+//   let users = await getUsers();
+//   console.log("Execution is completed");
+// }
+// main();
